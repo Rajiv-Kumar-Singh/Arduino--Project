0a4bc8253f0a0c746b60210b8a9c0cf2b9737c3b
Rajiv-Kumar-Singh <rv488448@gmail.com>
2021-06-24 01:53:38 +0530

    adding files

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ var fs = require('fs');
 var index = fs.readFileSync( 'index.html');
 
 
-var SerialPort = require('serialport');
-const parsers = SerialPort.parsers;
+// var SerialPort = require('serialport');
+// const parsers = SerialPort.parsers;
 
-const parser = new parsers.Readline({
-    delimiter: '\r\n'
-});
+// const parser = new parsers.Readline({
+//     delimiter: '\r\n'
+// });
 
-var port = new SerialPort('COM3',{ 
-    baudRate: 9600,
-    dataBits: 8,
-    parity: 'none',
-    stopBits: 1,
-    flowControl: false
-});
+// var port = new SerialPort('COM3',{ 
+//     baudRate: 9600,
+//     dataBits: 8,
+//     parity: 'none',
+//     stopBits: 1,
+//     flowControl: false
+// });
 
-port.pipe(parser);
+// port.pipe(parser);
 
 var app = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
@@ -26,20 +26,20 @@ var app = http.createServer(function(req, res) {
     // res.send(data);
 });
 
-var io = require('socket.io').listen(app);
+// var io = require('socket.io').listen(app);
 
-io.on('connection', function(socket) {
+// io.on('connection', function(socket) {
     
-    console.log('Node is listening to port');
+//     console.log('Node is listening to port');
     
-});
+// });
 
-parser.on('data', function(data) {
+// parser.on('data', function(data) {
     
-    console.log('Received data from port: ' + data);
+//     console.log('Received data from port: ' + data);
     
-    io.emit('data', data);
+//     io.emit('data', data);
     
-});
+// });
 
 app.listen(process.env.PORT || 3000);
